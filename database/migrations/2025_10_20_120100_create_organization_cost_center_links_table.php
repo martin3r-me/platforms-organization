d5e9b7a6fd9bbdf3601de61a99a2f1809ddf5208
Martin Erren <martin3r@Mac.fritz.box>
2025-11-01 11:16:55 +0100

    cost center links

--- database/migrations/2025_10_20_120100_create_organization_cost_center_links_table.php
+++ database/migrations/2025_10_20_120100_create_organization_cost_center_links_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('organization_cost_center_links')) {
+            return;
+        }
+
         Schema::create('organization_cost_center_links', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->unique();
